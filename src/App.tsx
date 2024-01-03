bbf8d919cf74254c6b401837eb7e16ff10433c85
nimroddanielmaayan <nimrodmayan@gmail.com>
2024-01-03 14:37:25 +0200

    commit daily progress

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
       <h1 className="text-center text-xl font-semibold text-blue-950">
         Node Shop
       </h1>
+      <div>
+        <p className="bg-blue-500 text-center text-white">Example text...</p>
+      </div>
     </>
   );
 }
